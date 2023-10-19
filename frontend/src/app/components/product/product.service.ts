812369bb7004dec174a0798678227b63a855cfe5
fabio-mattos <softvision.fpolis@gmail.com>
2023-10-19 09:50:06 -0300

    Update title pages

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { MatSnackBar} from  '@angular/material/snack-bar';
 import {  HttpClient } from '@angular/common/http';
 import { Product } from './product.model';
-import { Observable } from 'rxjs';
+import { EMPTY, Observable, catchError, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,38 +12,58 @@ export class ProductService {
   baseUrl = "http://localhost:3001/products"
   constructor(private snackBar: MatSnackBar, private http:HttpClient) { }
 
-showMessage(msg: string): void {
+showMessage(msg: string, isError: boolean = false ): void {
    this.snackBar.open(msg, 'X', {
     duration: 3000,
     horizontalPosition: "right",
-    verticalPosition: "top"
+    verticalPosition: "top",
+    panelClass: isError ? ['msg-error'] : ['msg-success']
    })
 }
 
 create(product: Product): Observable<Product> {
-  return this.http.post<Product>(this.baseUrl, product)
+  return this.http.post<Product>(this.baseUrl, product).pipe(
+    map(obj => obj), 
+    catchError(e => this.errorHandler(e,'Erro ao salvar o registro!'))
+  )
 }
 
-
 read(): Observable<Product[]> {
-  return this.http.get<Product[]>(this.baseUrl)
+  return this.http.get<Product[]>(this.baseUrl).pipe(
+    map(obj => obj), 
+    catchError(e => this.errorHandler(e,'Erro ao buscar os registros!'))
+  )
 }
 
 
 readById(id: number): Observable<Product> {
    const url = `${this.baseUrl}/${id}`
-   return this.http.get<Product>(url)
+   return this.http.get<Product>(url).pipe(
+    map(obj => obj), 
+    catchError(e => this.errorHandler(e,'Erro ao localisar o registro pelo ID!'))
+   )
 }
 
 
 update(product: Product): Observable<Product>{
   const url = `${this.baseUrl}/${product.id}`
-  return this.http.put<Product>(url, product)
+  return this.http.put<Product>(url, product).pipe(
+    map(obj => obj), 
+    catchError(e => this.errorHandler(e,'Erro ao atualizar o registro!'))
+  )
 }
 
 delete(id: number): Observable<Product> {
   const url = `${this.baseUrl}/${id}`
-  return this.http.delete<Product>(url)
+  return this.http.delete<Product>(url).pipe(
+    map(obj => obj), 
+    catchError(e => this.errorHandler(e,'Erro ao deletar o registro!'))
+  )
+}
+
+errorHandler(e: any, msg: string): Observable<any> {
+  this.showMessage(msg,true)
+  return EMPTY
 }
 
 }
---
+++ frontend/src/app/components/template/header/header.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HeaderData } from './header-data.model';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeaderService {
+/**  O Subject é o observador para notificar alguma mudança */
+  private _headerData =  new BehaviorSubject<HeaderData>(({
+    title: 'Início',
+    icon: 'home',
+    routerUrl: ''
+  }))
+
+  constructor() { }
+
+get headerData(): HeaderData {
+  return this._headerData.value
+}
+
+
+set headerData(heardData: HeaderData) {
+  this._headerData.next(heardData)
+}
+
+}
